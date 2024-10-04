dba12f6b4a788ec8c6eb2c746233506e409e1610
Natalie Aoya <77142187+Natalie-A@users.noreply.github.com>
2024-10-04 11:13:33 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,17 @@
 set -e
 
 # Create Kind cluster
-sudo kind create cluster --name webapp || { echo "Kind cluster already exists."; }
+sudo kind create cluster --name webapp
 
 # Load Docker image into Kind
 sudo docker pull natalieaoya/flask.webapp:latest
 sudo kind load docker-image natalieaoya/flask.webapp:latest --name webapp
 
-# Apply Kubernetes manifests
-kubectl cluster-info --context kind-webapp
+# Apply Kubernetes manifest
+mkdir ~/.kube
+sudo kind get kubeconfig --name webapp > ~/.kube/config
 kubectl apply -f k8s-deployment.yaml
+kubectl port-forward svc/flask-service 8080:80
 
 # Wait a few seconds for port forwarding to establish
 sleep 5
