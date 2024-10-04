8ca5389d76136c81836257da756a6faaf43a671a
Natalie Aoya <77142187+Natalie-A@users.noreply.github.com>
2024-10-04 08:15:29 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Create Kind cluster
+kind create cluster --name webapp || { echo "Kind cluster already exists."; }
+
+# Load Docker image into Kind
+sudo docker pull natalieaoya/flask.webapp:latest
+kind load docker-image natalieaoya/flask.webapp:latest --name webapp
+
+# Apply Kubernetes manifests
+kubectl apply -f k8s-deployment.yaml
+
+# Wait a few seconds for port forwarding to establish
+sleep 5
